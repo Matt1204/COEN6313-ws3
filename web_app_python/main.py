7ecbea8b5c93773bf05c21e6a39b4880915fd2fc
Matt1204 <90756450+Matt1204@users.noreply.github.com>
2024-10-01 17:30:10 -0400

    new route

--- web_app_python/main.py
+++ web_app_python/main.py
@@ -11,6 +11,12 @@ def hello_world():
     """Example Hello World route."""
 
     return f"Hello World!!!!!!"
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_world():
+    """Example Hello World route."""
+
+    return f"Hello New Route!!!"
     
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
